2ecc34dba0f238736e33aac4eab4ca3fa5d4f1e7
Vlad Gorenkov <vlad.gorenkov20@gmail.com>
2023-02-13 12:50:30 +0300

    add string representation to models and provide table names

--- transactions/models.py
+++ transactions/models.py
@@ -7,15 +7,26 @@ class Account(models.Model):
     title = models.TextField()
     current_amount = models.DecimalField(max_digits=19, decimal_places=2)
 
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        db_table = "accounts"
+
 class Transaction(models.Model):
 
-    class Account_types(models.TextChoices):
+    class Transaction_types(models.TextChoices):
         CONSUMPTION = 'consumption', 'Consumption'
         INCOME = 'income', 'Income'
 
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     amount = models.IntegerField()
-    type = models.CharField(max_length=15, choices=Account_types.choices, default=Account_types.CONSUMPTION)
+    type = models.CharField(max_length=15, choices=Transaction_types.choices, default=Transaction_types.CONSUMPTION)
     date = models.DateField(default=timezone.now)
     category = models.TextField(blank=True)
 
+    def __str__(self):
+        return f'Category: {self.category}, amount: {self.amount}, date: {self.date}'
+
+    class Meta:
+        db_table = "transactions"
